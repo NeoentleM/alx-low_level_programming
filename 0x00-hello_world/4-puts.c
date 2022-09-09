ccbdffb85239be4e38044d4ef1b4427e3c37801d
NeoentleM <neoentlemash64@mail.com>
2022-09-08 17:10:34 -0700

    program that prints a line in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
 /**
+ *main -Print line position
  *
- *  * main - Entry point
- *
- *   *
- *
-     * Return: Always 0 (Success)
- *
- *     */
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
 
 {
+		puts("\"Programming is like building a multilingual puzzle");
 
-			puts("Programming is like building a multilingual puzzle\n");
-			puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		return (0);
+
 }
