902a63d5dea7349c78aa40a9de426910df379a0d
NeoentleM <neoentlemash64@mail.com>
2022-09-14 11:21:44 -0700

    main.h file and README.md file created

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry Point
+ *
+ * Return: Always Success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+return (0)
+}
