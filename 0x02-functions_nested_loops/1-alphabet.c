7488d9b38bcf0122106c5065c980ee52c14a0014
NeoentleM <neoentlemash64@mail.com>
2022-09-28 16:43:20 -0700

    task 0 revision

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print letters of the alphabet
+ *
+ * Return: Always Success
+ */
+void print_alphabet(void)
+{
+int n;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+_putchar('/n');
+}
+
